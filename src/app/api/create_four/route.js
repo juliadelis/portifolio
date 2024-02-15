aa3509e2d9b6dfe08dbbe06f0708a156ee4a544b
Julia Delis <juliadelis2@hotmail.com>
2024-02-15 11:39:47 -0300

    fix: email bug fix e ajuste mobile

--- src/app/api/create_four/route.js
+++ src/app/api/create_four/route.js
@@ -1,17 +1,24 @@
 import { sendMail } from "../../../services/email";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req, res) {
-  const { email, subject, mensagem } = await req.json();
-  const response = await sendMail({
-    from: email,
-    to: "juliadelis2@gmail.com",
-    subject,
-    html: `<h2>Email:</h2> <p>${email}</p>
-    <br/>
-   <h2> Mensagem: </h2> <p>${mensagem}</p>`,
-  });
-  console.log(response);
-  return Response.json({ message: "success" });
+  try {
+    const { email, subject, mensagem } = await req.json();
+
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "juliadelis2@gmail.com",
+      subject,
+      html: `<h2>Email:</h2> <p>${email}</p>
+  <br/>
+ <h2> Mensagem: </h2> <p>${mensagem}</p>`,
+    });
+    return Response.json({ message: "success" });
+  } catch (error) {
+    return Response.json({ message: "error", error });
+  }
 }
 
 export async function GET(request, res) {
