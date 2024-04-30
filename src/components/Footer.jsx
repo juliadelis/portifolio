692c5f2862667b7989846e77409417859b8c5735
Julia Delis <juliadelis2@hotmail.com>
2024-04-30 09:33:13 -0300

    fix: mobile footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="footer border border-t-[#33353f] border-l-transparent border-b-transparent border-r-transparent text-white">
-      {' '}
+      {" "}
       <div className="container p-12 flex justify-between">
         <Image
-          className="rounded mb-4 lg:mb-0"
+          className="rounded mb-4 mr-8 lg:mr-0 lg:mb-0"
           src="/images/jdelis.svg"
           width={100}
           height={100}
@@ -15,7 +15,7 @@ const Footer = () => {
         <p className="text-slate-600">All Rights reserved.</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
