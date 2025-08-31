4322d3979ecfd35ec3a7f531813dbcdf5ddb3bc8
Julia Delis <juliadelis2@hotmail.com>
2025-08-31 01:24:00 -0300

    feat: new hero setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import HeroSection from "../components/HeroSection.jsx";
+import HeroSectionNew from "../components/HeroSectionNew.jsx";
 import Navbar from "../components/Navbar.jsx";
 import AboutSection from "../components/AboutSection.jsx";
 import ProjectsSection from "../components/ProjectsSection.jsx";
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
+      <div className="container mt-24 mx-auto py-4">
+        <HeroSectionNew />
         <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
